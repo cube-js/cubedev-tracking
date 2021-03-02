849e48ef813ec12b46be30e1c3c41957b5f59409
TrapenokVictor <Levhav@yandex.ru>
2021-03-02 18:14:21 +1000

    feat: iframe id

--- src/index.js
+++ src/index.js
@@ -8,57 +8,21 @@ let baseProps = {};
 const COOKIE_ID = "cubedev_anonymous";
 const COOKIE_DOMAIN = ".cube.dev";
 const MAX_AGE = 365 * 24 * 60 * 60 * 1000; // 1 year
-
-function getCookie(name) {
-  var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ))
-  const res = matches ? decodeURIComponent(matches[1]) : undefined
-  window.console.log(`getCookie: ${name}`, res)
-  debugger
-  return res;
-}
-
-function setCookie(name, value, options) {
-
-  window.console.log(`setCookie: ${name} = ${value}`, options)
-  const encode = function(value){
-    try {
-      return encodeURIComponent(value);
-    } catch (e) {
-      debug('error `encode(%o)` - %o', value, e)
-    }
-  }
-  options = options || {};
-  var str = encode(name) + '=' + encode(value);
-
-  if (null == value) options.maxage = -1;
-
-  if (options.maxage) {
-    options.expires = new Date(+new Date + options.maxage);
-  }
-
-  if (options.path) str += '; path=' + options.path;
-  if (options.domain) str += '; domain=' + options.domain;
-  if (options.expires) str += '; expires=' + options.expires.toUTCString();
-  if (options.secure) str += '; secure';
-  if (options.sameSite) str += '; SameSite=' + options.sameSite;
-
-  document.cookie = str;
-}
+let clientAnonymousId = undefined;
 
 const track = async (event) => {
-  // debugger
-  if (!getCookie(COOKIE_ID)) {
-    setCookie(COOKIE_ID, uuidv4(), { domain: COOKIE_DOMAIN, maxage: MAX_AGE, sameSite: 'None', secure:true, path:'/' });
+  if(!clientAnonymousId){
+    window.console.log("waite clientAnonymousId from iframe")
+    return setTimeout(track, 1000, event)
   }
+
   trackEvents.push({
     ...baseProps,
     ...event,
     referrer: document.referrer,
     ...window.location,
     id: uuidv4(),
-    clientAnonymousId: getCookie(COOKIE_ID),
+    clientAnonymousId,
     clientTimestamp: new Date().toJSON()
   });
   const flush = async (toFlush, retries) => {
@@ -138,8 +102,8 @@ Object.defineProperty(cubeTrack, "anonymous", {
 });
 
 window.onmessage = function(event){
-  debugger
-  console.log("onmessage:", event.data)
+  console.log("get clientAnonymousId:", event.data)
+  clientAnonymousId = event.data.clientAnonymousId
 
   if(window.cubeTrack.page){
     page(window.cubeTrack.page)
